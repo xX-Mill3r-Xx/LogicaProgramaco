951f8d600f484dcd3ad8b7b9f34a522ec476ec71
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-02-25 14:52:49 -0300

    145225022022

---
+++ Casting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            double z = 0.1;
+
+            x = 5;
+            y = 10;
+            z = x + y;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            // para fazer uma variavel double receber uma variavel int;
+            y = (int)z;
+            Console.WriteLine(y);
+
+            Console.ReadKey();
+        }
+    }
+}
