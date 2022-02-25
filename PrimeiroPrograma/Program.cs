7cfbb9f9a0b6ffee6e65bc0d16ef63c7c86414e2
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-02-25 14:36:22 -0300

    143625022022

---
+++ PrimeiroPrograma/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrimeiroPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com seu nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine($"O nome digitado é: {nome}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Variaveis/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração da sintaxe de uma vatiavel;
+            //<TIPO> <NOME> = <VALOR INICIAL>;
+
+            string nome = "Seu Nome"; //Exemplo de uma variavel que armazena texto;
+            int numero = 10; //Exemplo de uma variavel que armazena numeros inteiros;
+            float numeroFlutuande = 1.85f; //Exemplo de uma variavel que armazena numeros de ponto flutuante;
+            /*Obs.: variaveis do tipo float precisão ser sinalizadas com a letra f no final da atribuição*/
+            double numeroComPonto = 0.1; //Variaveis que armazena numeros com casas decimais;
+            char letra = 'L'; //Variaveis que armazena um caracter;
+
+            Console.WriteLine(nome);
+            Console.WriteLine(numero);
+            Console.WriteLine(numeroFlutuande);
+            Console.WriteLine(numeroComPonto);
+            Console.WriteLine(letra);
+
+            Console.ReadKey();
+        }
+    }
+}
