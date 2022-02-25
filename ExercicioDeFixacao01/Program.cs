225b5c22c74e2806384258672226e23d6dd7cd8c
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-02-25 19:58:40 -0300

    195825022022

---
+++ ExercicioDeFixacao01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixacao01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com seu nome completo: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Quantos quartos tem em sua casa? ");
+            int quartos = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o preço de um produto: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com seu ultimo nome, idade e altura (na mesma linha):");
+
+            string[] dados = Console.ReadLine().Split(' ');
+            string ultimoNome = dados[0];
+            int idade = int.Parse(dados[1]);
+            double altura = double.Parse(dados[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine(ultimoNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
